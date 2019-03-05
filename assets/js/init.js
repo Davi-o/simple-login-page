30bf7538954e0600261cace6290fd9e49e999823
Davi Alves <alvesdelima.davi@gmail.com>
2019-03-05 05:22:03 -0300

    a bit more stylized

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $('.modal').modal();
-  });
+    $('.carousel').carousel();
+});
+
+function smoothScrollBegin(){
+  var element = document.getElementById("inicio");
+  element.scrollIntoView({behavior: "smooth", block: "start"});
+}
+
+function smoothScrollContact(){
+  var element = document.getElementById("contato");
+  element.scrollIntoView({behavior: "smooth", block: "end"});
+}
--- view/index.php
+++ view/index.php
@@ -8,6 +8,7 @@
     <title>Sistema de Login</title>
     <link rel="stylesheet" href="../assets/css/materialize.css" />
     <link rel="stylesheet" href="../assets/css/colors.css" />
+    <link rel="stylesheet" href="../assets/css/mycss.css" />
     <link rel="stylesheet" type="text/css" href="../assets/css/login.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.js"></script>
     <script src="../assets/js/materialize.js"></script>
@@ -16,21 +17,34 @@
 
 <body>
     <header>
-        <nav class="grey darken-4">
-            <div>
-                <ul>
-                    <li class="menu-item"><a href="">Inicio</a></li>
-                    <li class="menu-item"><a href="">Fim</a></li>
-                    <li><a class="btn modal-trigger" href="#modal">Login</a></li>
-                </ul>
-            </div>
-        </nav>
+        <div id="inicio" class="navbar-fixed">
+            <nav class="grey darken-4">
+                <div class="nav-wrapper">
+                    <ul class="right">
+                        <li class="menu-item"><a onclick="smoothScrollBegin()" href="#!">Inicio</a></li>
+                        <li class="menu-item"><a onclick="smoothScrollContact()">Contatos</a></li>
+                        <li><a class="btn modal-trigger" href="#modal">Login</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </header>
     <section>
-        <div class="card-panel">
-            <div class="card-content">
-                <p>AAAAA</p>
+        <div class="blue-grey darken-4">
+
+            <div class="bcg-img center flow-text">
+                <div class="txt">
+                    <h1>Bem Vindo</h1>
+                    <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo
+                        a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os
+                        caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos,
+                        mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi
+                        popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens
+                        com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que
+                        incluem versões do Lorem Ipsum.</p>
+                </div>
             </div>
+
         </div>
         <div id="modal" class="modal blue-grey darken-3">
             <div class="modal-content">
@@ -47,15 +61,39 @@
                 unset($_SESSION['falha_de_autenticacao']);
                 endif;
                 ?>
-                <form href="#!"action="../dao/login.php" method="POST">
-                    <input name="usuario" name="text" class="teal-text" placeholder="Seu usuário" autofocus="" required>
+                <form href="#!" action="../dao/login.php" method="POST">
+                    <input name="usuario" name="text" class="teal-text" placeholder="Seu usuário" required>
                     <input name="senha" class="teal-text" type="password" placeholder="Sua senha" required>
                     <button type="submit" class="btn">Entrar</button>
                 </form>
             </div>
         </div><!-- Login -->
     </section>
-    <footer>
+    <footer class="page-footer grey darken-3">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Footer Content</h5>
+                    <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer
+                        content.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Links</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright grey darken-4">
+            <div id="contato" class="container">
+                © 2014 Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+        </div>
     </footer>
 </body>
 
