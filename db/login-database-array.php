cc9e127087f8ca9d1b8f431ba78712486d32bb85
Davi Alves <alvesdelima.davi@gmail.com>
2019-03-06 01:37:06 -0300

    database commit

---
+++ db/login-database-array.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.8.2
+ */
+
+/**
+ * Database `login`
+ */
+
+/* `login`.`usuario` */
+$usuario = array(
+  array('id_usuario' => '1','nome_usuario' => 'davi','senha' => '202cb962ac59075b964b07152d234b70','email' => 'alvesdelima.davi@gmail.com','telefone' => '51985276555'),
+  array('id_usuario' => '2','nome_usuario' => 'Daniel','senha' => '202cb962ac59075b964b07152d234b70','email' => 'daniel@paladi','telefone' => '123456789456'),
+  array('id_usuario' => '3','nome_usuario' => 'mariana','senha' => 'e60408e9a55027070e3caf0550d2b4df','email' => 'daniel@paladi','telefone' => '123456789')
+);
---
+++ db/login-database-sql.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 06-Mar-2019 às 05:34
+-- Versão do servidor: 10.1.34-MariaDB
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome_usuario` varchar(200) NOT NULL,
+  `senha` char(32) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telefone` char(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nome_usuario`, `senha`, `email`, `telefone`) VALUES
+(1, 'davi', '202cb962ac59075b964b07152d234b70', 'alvesdelima.davi@gmail.com', '51985276555'),
+(2, 'Daniel', '202cb962ac59075b964b07152d234b70', 'daniel@paladi', '123456789456'),
+(3, 'mariana', 'e60408e9a55027070e3caf0550d2b4df', 'daniel@paladi', '123456789');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
