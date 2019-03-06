7add674ae8cfd8c51b80344f0a14e4118b354383
Davi Alves <alvesdelima.davi@gmail.com>
2019-03-06 01:38:24 -0300

    Update login-database-array.php

--- db/login-database-array.php
+++ db/login-database-array.php
@@ -11,6 +11,6 @@
 /* `login`.`usuario` */
 $usuario = array(
   array('id_usuario' => '1','nome_usuario' => 'davi','senha' => '202cb962ac59075b964b07152d234b70','email' => 'alvesdelima.davi@gmail.com','telefone' => '51985276555'),
-  array('id_usuario' => '2','nome_usuario' => 'Daniel','senha' => '202cb962ac59075b964b07152d234b70','email' => 'daniel@paladi','telefone' => '123456789456'),
-  array('id_usuario' => '3','nome_usuario' => 'mariana','senha' => 'e60408e9a55027070e3caf0550d2b4df','email' => 'daniel@paladi','telefone' => '123456789')
+  array('id_usuario' => '2','nome_usuario' => 'daniel','senha' => '202cb962ac59075b964b07152d234b70','email' => 'daniel@paladino','telefone' => '123456789456'),
+  array('id_usuario' => '3','nome_usuario' => 'marcos','senha' => 'e60408e9a55027070e3caf0550d2b4df','email' => 'marcos@paladino','telefone' => '123456789')
 );
